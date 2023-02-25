5e5492ac0f8e03789b2ee8463d7a7676b1c41b75
LiudmylaStarovoit <103971713+LiudmylaStarovoit@users.noreply.github.com>
2023-02-25 15:02:50 +0100

    Add files via upload

--- js/crew.js
+++ js/crew.js
@@ -20,7 +20,7 @@ commander.addEventListener('click', () => {
     subTitle.textContent = 'Commander';
     mainTitleCrew.textContent = 'Douglas Hurley';
     textCrew.textContent = 'Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.';
-    image.src = "./assets/crew/image-douglas-hurley.png";
+    image.src = "./image/crew/image-douglas-hurley.png";
     if (window.matchMedia("(min-width: 577px)").matches) {
         photo.style.width = '520px';
         photo.style.height = '695px';
@@ -33,7 +33,7 @@ specialist.addEventListener('click', () => {
     subTitle.textContent = 'Mission Specialist';
     mainTitleCrew.textContent = 'MARK SHUTTLEWORTH';
     textCrew.textContent = 'Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.';
-    image.src = "./assets/crew/image-mark-shuttleworth.png";
+    image.src = "./image/crew/image-mark-shuttleworth.png";
     if (window.matchMedia("(min-width: 577px)").matches) {
         photo.style.width = '450px';
         photo.style.height = '638px';
@@ -46,7 +46,7 @@ pilot.addEventListener('click', () => {
     subTitle.textContent = 'PILOT';
     mainTitleCrew.textContent = 'Victor Glover';
     textCrew.textContent = 'Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64.';
-    image.src = "./assets/crew/image-victor-glover.png";
+    image.src = "./image/crew/image-victor-glover.png";
     if (window.matchMedia("(min-width: 577px)").matches) {
         photo.style.width = '548px';
         photo.style.height = '665px';
@@ -59,7 +59,7 @@ engineer.addEventListener('click', () => {
     subTitle.textContent = 'Flight Engineer';
     mainTitleCrew.textContent = 'Anousheh Ansari';
     textCrew.textContent = 'Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.';
-    image.src = "./assets/crew/image-anousheh-ansari.png";
+    image.src = "./image/crew/image-anousheh-ansari.png";
     if (window.matchMedia("(min-width: 577px)").matches) {
         photo.style.width = '564.57px';
         photo.style.height = '600px';
--- js/script.js
+++ js/script.js
@@ -65,7 +65,7 @@ moon.addEventListener('click', () => {
     text.textContent = 'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.';
     distance.textContent = '384,400 km';
     travel.textContent = '3 days';
-    image.src = './assets/destination/image-moon.png';
+    image.src = './image/destination/image-moon.png';
 
 }); 
 mars.addEventListener('click', (e) => {
@@ -73,21 +73,21 @@ mars.addEventListener('click', (e) => {
     text.textContent = 'Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!';
     distance.textContent = '225 MIL. km';
     travel.textContent = '9 months';
-    image.src = './assets/destination/image-mars.png';
+    image.src = './image/destination/image-mars.png';
 }); 
 europa.addEventListener('click', (e) => {
     mainTitle.textContent = 'EUROPA';
     text.textContent = 'The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.';
     distance.textContent = '628 MIL. km';
     travel.textContent = '3 years';
-    image.src = './assets/destination/image-europa.png';
+    image.src = './image/destination/image-europa.png';
 }); 
 titan.addEventListener('click', (e) => {
     mainTitle.textContent = 'TITAN';
     text.textContent = 'The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.';
     distance.textContent = '1.6 BIL. km';
     travel.textContent = '7 years';
-    image.src = './assets/destination/image-titan.png';
+    image.src = './image/destination/image-titan.png';
 }); 
 
 
